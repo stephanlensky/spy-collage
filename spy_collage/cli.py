c630d24cf236bcd3bbdb09cee559fa12dd1d4ca8
Stephan Lensky <stephanl.public@gmail.com>
2021-10-15 18:30:45 -0400

    Project skeleton

---
+++ spy_collage/cli.py
@@ -0,0 +1,12 @@
+import click
+
+
+@click.command()
+def cli():
+    """Configurable Python album art collage generator for Spotify, featuring album discovery and
+    color clustering."""
+    print("hello world")
+
+
+if __name__ == "__main__":
+    cli()
